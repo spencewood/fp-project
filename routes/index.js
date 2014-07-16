2538d72e254f508a9ef6ccd06b33cc87bd04a6d8
Tyler Spencewood <tspencewood@gmail.com>
2014-07-16 10:53:09 -0500

    Add pubnub subscription.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var pubnub = require('pubnub').init({
+  publish_key: process.env.PUBNUB_PUBLISH_KEY || 'demo',
+  subscribe_key: process.env.PUBNUB_SUBSCRIBE_KEY || 'demo'
+});
+
+pubnub.subscribe({
+  channel: 'fp-demo',
+  callback: function(message){
+    console.log(message);
+  }
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* POST add project */
+router.post('/projects', function(req, res) {
+  pubnub.publish({
+    channel: 'fp-demo',
+    message: {
+      title: req.body.title,
+      subtitle: req.body.subtitle,
+      body: req.body.body
+    }
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
